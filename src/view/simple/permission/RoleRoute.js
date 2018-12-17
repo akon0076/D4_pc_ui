46a4d3fc6da965d79091246ffc9c6b82267de644
cbg <1404510094@qq.com>
2018-12-17 19:58:00 +0800

    更新分配权限

--- src/view/simple/permission/RoleRoute.js
+++ src/view/simple/permission/RoleRoute.js
@@ -1,46 +1,54 @@
-
-
 //角色管理
 
 export default [
-{
+  {
     path: '/simple/permission/Role',
     name: 'simple_permission_Role',
     meta: {
-        label: '组织角色',
-        icon: 'icon iconfont icon-fl-origin',
-        requireAuth: true,
+      label: '组织角色',
+      icon: 'icon iconfont icon-fl-origin',
+      requireAuth: true,
     },
     component: () => import('./RoleList')
-},
-{
+  },
+  {
     path: '/simple/permission/Role/add',
     name: 'simple_permission_Role_ddd',
     meta: {
-        label: '新增角色',
-        icon: 'icon iconfont icon-fl-origin',
-        requireAuth: true,
+      label: '新增角色',
+      icon: 'icon iconfont icon-fl-origin',
+      requireAuth: true,
     },
     component: () => import('./RoleForm')
-},
-{
+  },
+  {
     path: '/simple/permission/Role/edit/:roleId',
     name: 'simple_permission_Role_edit',
     meta: {
-        label: '编辑角色',
-        icon: 'icon iconfont icon-fl-origin',
-        requireAuth: true,
+      label: '编辑角色',
+      icon: 'icon iconfont icon-fl-origin',
+      requireAuth: true,
     },
     component: () => import('./RoleForm')
-},
-{
+  },
+  {
     path: '/simple/permission/Role/display/:roleId',
     name: 'simple_permission_Role_display',
     meta: {
-        label: '查看角色',
-        icon: 'icon iconfont icon-fl-origin',
-        requireAuth: true,
+      label: '查看角色',
+      icon: 'icon iconfont icon-fl-origin',
+      requireAuth: true,
     },
     component: () => import('./RoleDisplay')
-}
+  },
+  {
+    path: '/simple/permission/Role/permissionAssignment/:roleId',
+    name: 'simple_permission_Role_permissionAssignment',
+    meta: {
+      label: '分配权限',
+      icon: 'icon iconfont icon-fl-origin',
+      requireAuth: true,
+    },
+    component: () => import('./PermissionAssignment')
+  }
 ]
